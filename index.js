f160d76d63d4fabdf22282c42c2ae7a85df08da5
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-10-04 00:11:57 +0530

    add jwt in auth project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const path = require('path');
 const cookieparser=require('cookie-parser');
 const User=require('./models/User')
+const bcrypt = require('bcrypt');
+const jwt=require('jsonwebtoken')
 const app=express();
 
 app.use(express.json());
@@ -13,16 +15,55 @@ app.use(cookieparser());
 app.get('/',(req,res)=>{
     res.render('index');
 })
-app.post('/create',async (req,res)=>{
+app.post('/create', (req,res)=>{
     let {username,email,password,age}=req.body;
-    let createUser=await User.create({
+        bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt, async(err,hash)=>{  
+            console.log(hash);
+        let createUser=await User.create({
         username,
         email,
-        password,
+        password:hash,  
         age
     })
-    res.send(createUser)
+    
+        let token=jwt.sign({email},"secrate-key");
+        res.cookie("token",token);
+        res.send(createUser)
+            
+        })
+       
+    })
+
+});
+
+app.get("/login",function(req,res){
+    res.render('Login');
 })
+
+app.post("/login",async function(req,res){
+  let user=await User.findOne({email:req.body.email});
+  console.log(user);
+    if(!user){
+        res.send("Something went wrong");
+    }
+    //compare password db and user input
+    bcrypt.compare(req.body.password,user.password,function (error,result){
+        if(result){
+            let token=jwt.sign({email:user.email},'secrate-key');
+            res.cookie("token",token);
+            
+        res.send("Yes You Can login");
+        }
+        else
+         res.send("Something went wrong plse check email password");
+    })
+    
+})
+
+app.post('/logout',function(req,res){
+    res.cookie("token",'');
+});
 
 app.listen(3000,()=>{
     console.log(`server Runing on PORT ${3000}`);
